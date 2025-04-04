b3d6a48415f87865364cfc9d66a98e2afd9099ef
Simon Y. Blackwell <syblackwell@anywhichway.com>
2025-04-04 07:30:11 -0700

    Documentation edits made through Mintlify web editor

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded",event => {
   console.log(event);
-  const poweredByEl = document.querySelector('a[href*="poweredBy"]');
-  poweredByEl.style.display = "none";
+  //const poweredByEl = document.querySelector('a[href*="poweredBy"]');
+  //if(poweredByEl) poweredByEl.style.display = "none";
   const audioEls= document.querySelectorAll("img");
   audioEls.forEach(el => {
     const url = new URL(el.src,"https://raw.githubusercontent.com/anywhichway/mintlify-docs/refs/heads/main/assets/audio/");
     url.pathname = url.pathname.split(".")[0] + ".mp3"
+    console.log(url);
     const html = `<iframe srcdoc='<html><body style="margin:0;"><audio controls><source src="${url.href}" type="audio/mpeg" />Your browser does not support the audio element.</audio></body></html>' 
         width="300" 
         height="60" 
