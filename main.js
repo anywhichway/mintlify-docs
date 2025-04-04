36516bd5787dfac2c8e1ac4fc1d2e95bd036256b
Simon Y. Blackwell <syblackwell@anywhichway.com>
2025-04-04 08:43:47 -0700

    Documentation edits made through Mintlify web editor

--- main.js
+++ main.js
@@ -11,20 +11,22 @@ window.addEventListener('popstate', (event) => {
   console.log('State changed:', window.location.hash);
 });
 console.log('Loaded ',window.location);
-//setTimeout(() => {
-  //console.log("Running timeout...")
-    const audioEls= document.querySelectorAll("img");
-    console.log(audioEls)
-  audioEls.forEach(el => {
-    const url = new URL(el.src,"https://raw.githubusercontent.com/anywhichway/mintlify-docs/refs/heads/main/assets/audio/");
-    url.pathname = url.pathname.split(".")[0] + ".mp3"
-    console.log(url);
-    const html = `<iframe srcdoc='<html><body style="margin:0;"><audio controls><source src="${url.href}" type="audio/mpeg" />Your browser does not support the audio element.</audio></body></html>' 
-        width="300" 
-        height="60" 
-        frameborder="0">
-      </iframe>`;
-    el.insertAdjacentHTML("afterend",html);
-  });
-  
- // },2000)
+if(!window.watchContent) {
+  window.watchContent = setInterval(() => {
+      const audioEls= document.querySelectorAll("img.icendant-example");
+      console.log(audioEls)
+    audioEls.forEach(el => {
+      if(!el.nextSibilingElement?.tagName==="IFRAME") {
+        const url = new URL(el.src,"https://raw.githubusercontent.com/anywhichway/mintlify-docs/refs/heads/main/assets/audio/");
+      url.pathname = url.pathname.split(".")[0] + ".mp3"
+      console.log(url);
+      const html = `<iframe srcdoc='<html><body style="margin:0;"><audio controls><source src="${url.href}" type="audio/mpeg" />Your browser does not support the audio element.</audio></body></html>' 
+          width="300" 
+          height="60" 
+          frameborder="0">
+        </iframe>`;
+      el.insertAdjacentHTML("afterend",html);
+      }
+    });
+  },1000)
+}
