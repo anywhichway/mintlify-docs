1feead4299a24d1c03b5ded9301eb92ac2134fb4
syblackwell <syblackwell@anywhichway.com>
2025-04-13 04:26:12 -0700

    initial commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ if(!window.watchContent) {
             debugger;
             audioEls.forEach(el => {
                 if(el.nextElementSibling?.tagName=="IFRAME") return;
-                const url = new URL(el.getAttribute("src"));
+                const url = new URL(el.getAttribute("src"),window.location);
                 const filename = (url.pathname.split(".")[0] + ".mp3").split("/").pop();
                 const href = "https://raw.githubusercontent.com/anywhichway/mintlify-docs/refs/heads/main/assets/audio/" +  filename;
                 const html = `<iframe srcdoc='<html><body style="margin:0;"><audio controls><source src="${href}" type="audio/mpeg" />Your browser does not support the audio element.</audio></body></html>' 
