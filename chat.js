8877340a5de2cfb44e917f892b81ec4cb4029597
syblackwell <syblackwell@anywhichway.com>
2025-05-13 08:34:39 -0700

    initial commit

---
+++ chat.js
@@ -0,0 +1,7 @@
+const head = document.querySelector("head");
+const script = document.createElement("script");
+script.setAttribute("defer", "");
+script.setAttribute("type","module");
+script.setAttribute("src","https://embeddables.p.mbirdcdn.net/v1/web-components.es.js");
+head.appendChild(script);
+
