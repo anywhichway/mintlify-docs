98a8b3ecc6e6f535d140f865f7582b0a2eb85564
syblackwell <syblackwell@anywhichway.com>
2025-05-21 17:24:41 -0700

    initial commit

--- chat.js
+++ chat.js
@@ -13,26 +13,30 @@ setTimeout( () => {
         const style = document.createElement("style");
         style.innerText = '[title="Attach file"] { display: none; }';
         setTimeout(() => {
-            chat.shadowRoot.appendChild(style)
+            debugger;
+            chat.shadowRoot.appendChild(style);
+            const orginalFetch = window.fetch;
+            window.fetch = async function(...args) {
+                try {
+                    const url = typeof args[0]==="string" ? new URL(args[0]) : new URL(args[0].url);
+                    if(url.hostname==="api.bird.com") {
+                        console.log(args);
+                        const response = orginalFetch(...args);
+                        const clonedResponse = response.clone();
+                        clonedResponse.text().then(text => {
+                            console.log(url.href,text);
+                        })
+                        return response;
+                    }
+                } catch {
+                    // ignore
+                }
+                return orginalFetch(...args);
+            }
         },500);
     }
     // <messagebird-chat project-id="4d74e764-1999-4c19-887b-a2aa814c91ee" workspace-id="6645e1bc-6b95-4955-a79f-573a0cb9f27b"></messagebird-chat>
 },2000);
 
-(() => {
-    const orginalFetch = window.fetch;
-    window.fetch = async function(...args) {
-        const url = typeof args[0]==="string" ? new URL(args[0]) : new URL(args[0].url);
-        if(url.hostname==="api.bird.com") {
-            console.log(args);
-            const response = orginalFetch(...args);
-            const clonedResponse = response.clone();
-            clonedResponse.text().then(text => {
-                console.log(url.href,text);
-            })
-            return response;
-        }
-        return orginalFetch(...args);
-    }
-})()
+
 
